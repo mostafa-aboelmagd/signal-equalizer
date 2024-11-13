58271b1c5c8d896a65c88054f3e4842c7ce2b18b
Zisco2002 <zisco2002@gmail.com>
2024-11-13 02:12:55 +0200

    plot the signal

--- classes.py
+++ classes.py
@@ -10,17 +10,21 @@ from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
 from PyQt5.QtCore import QUrl
 
 import copy
+from UITEAM15 import Ui_MainWindow  # Import the Ui_MainWindow class
 
-class FileBrowser:
+class FileBrowser(Ui_MainWindow):
     global player
-    def __init__(self, parent):
+    def __init__(self, parent = None):
         self.parent = parent
         # Define the instruments and volumes
+        
       
     def browse_file(self, mode):
-        pass
-
-
+        if mode == 'music':
+            file_path, _ = QFileDialog.getOpenFileName(directory= "D:/Signal-Equalizer-DSP", filter= " wav files (*.wav)")
+            signal, sampling_rate= librosa.load(file_path, sr= None)
+            print(f"Signal path: {file_path}")
+            return signal, sampling_rate
 
   
 
--- main.py
+++ main.py
@@ -9,11 +9,12 @@ import matplotlib.pyplot as plt
 import plotly.graph_objs as go
 import plotly.offline as pyo
 import copy
+import librosa
  
 from PyQt5 import QtWidgets, QtCore, uic
 from PyQt5.QtWidgets import QMessageBox, QApplication, QVBoxLayout, QWidget
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
-from PyQt5.QtCore import QUrl
+from PyQt5.QtCore import QUrl, QTimer
 from PyQt5.QtGui import QIcon
 
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
@@ -27,7 +28,19 @@ class MainApp(QtWidgets.QMainWindow, Ui_MainWindow):
         super(MainApp, self).__init__()
         self.setupUi(self)
         self.setupUI()
+        self.retranslateUi(self)
+        self.timer = QTimer()
+        self.signal = []
+        self.modified_signal = []
+        self.sampling_rate = 0
+        self.chunksize = 10
+        self.curr_ptr = 0
+        self.segment_end = 0
+        self.right_x_limit = 4
+        self.isPaused = False
         self.connectSignals()
+        
+        
 
     def setupUI(self):
         """Setup UI elements and initial configurations."""
@@ -50,10 +63,11 @@ class MainApp(QtWidgets.QMainWindow, Ui_MainWindow):
         self.pushButton_zoomOut.clicked.connect(lambda: self.zoom(2))
         self.pushButton_reset.clicked.connect(lambda: self.stopAndReset(True))
         self.pushButton_stop.clicked.connect(lambda: self.stopAndReset(False))
+        self.timer.timeout.connect(self.plotSignal_timeDomain)
    
         # Connect other UI elements
         self.checkBox_showSpectrogram.stateChanged.connect(self.showAndHideSpectrogram)
-        self.comboBox_frequencyScale.activated.connect(self.setFrequencyScale)
+        #self.comboBox_frequencyScale.activated.connect(self.setFrequencyScale)
 
     def setMode(self, mode):
         """Set the mode of the application."""
@@ -61,12 +75,39 @@ class MainApp(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def uploadAndPlotSignal(self):
         """Upload and plot the signal."""
-        pass
+        file_browser = FileBrowser()
+        self.signal, self.sampling_rate = file_browser.browse_file(mode= 'music')
+        #self.plotSignal_timeDomain(self.sampling_rate, self.signal)
+        self.chunksize = 1000
+        self.curr_ptr = 0
+        self.left_x_view = 0 # used in adjusting the view of the signal while running in cine mode
+        self.time_values = np.linspace(0, 10, len(self.signal))
+        #self.plotSignal_timeDomain()
+        self.timer.start(100)
+        
 
-    def plotSignal_timeDomain(self, audio, audioSamplingRate, signal, widget):
+    def plotSignal_timeDomain(self):
         """Plot the signal in the time domain."""
-        pass
-
+        if len(self.signal) > 0 and self.isPaused == False:
+            # taking chunks from the signal and the corresponding time values
+            segment_y = self.signal[self.curr_ptr : self.curr_ptr + self.chunksize]   # from index "curr_ptr" to index "curr_ptr + chunksize"
+            segment_x = self.time_values[ self.curr_ptr : self.curr_ptr + self.chunksize]  # same in time values stored for the signal
+            
+            self.PlotWidget_inputSignal.plotItem.setYRange(-0.5, 0.5)
+            self.PlotWidget_inputSignal.plotItem.setXRange(self.left_x_view, self.left_x_view + 1)
+            self.PlotWidget_inputSignal.plot(segment_x, segment_y, pen = 'r')
+            
+            
+            if self.curr_ptr + self.chunksize <= len(self.signal):
+                self.curr_ptr += self.chunksize
+                if self.time_values[self.curr_ptr + self.chunksize] > self.left_x_view + 1:
+                    self.left_x_view += 1
+                
+            else:
+                self.timer.stop()
+                self.isPaused = True
+           
+           
     def plotSpectrogram(self, fig, canvas, audio, audioSamplingRate, signal):
         """Plot the spectrogram of the signal."""
         pass
@@ -75,41 +116,39 @@ class MainApp(QtWidgets.QMainWindow, Ui_MainWindow):
         """Plot the frequency spectrum of the signal."""
         pass
 
-    def computeFourierTransform(self):
-        """Compute the Fourier transform"""
-        pass
+   
 
     def computeFourierTransform(self):
         """Compute the Fourier transform of the signal."""
-        pass
+        self.fft_data = librosa.stft(self.signal)
+        self.magnitude, self.phase = librosa.magphase(self.fft_data)
+        
 
     def invFourierTransform(self, magnitude, phase):
         """Compute the inverse Fourier transform."""
-        pass
+        self.modified_fft_data = self.magnitude * self.phase
+        self.modified_signal = librosa.istft(self.modified_fft_data)
+        
 
-    def setFrequencyScale(self):
-        """Set the smoothing window."""
+    def updateNumOfSliders (self, mode = 'uniform'):
         pass
-
-    def setWindowParameters(self):
-        """Set the parameters for the smoothing window."""
-        pass
-
+    
+    
+    
     def getMappedSliderValue(self, slider_value):
         """Get the mapped value of the slider."""
         pass
 
-    def generateWindow(self, sliderNumber, Value):
-        """Generate the window for smoothing."""
-        pass
-
-    def applySmoothingWindow(self, gainList, targetBand):
-        """Apply the smoothing window."""
-        pass
 
     def togglePlayPause(self):
         """Toggle play/pause of the signal."""
-        pass
+        if self.isPaused == True:
+            self.isPaused = False
+            self.timer.start(100)
+        else:
+            self.isPaused = True
+            self.timer.stop()
+            
 
     def setSpeed(self, speed):
         """Set the playback speed."""
